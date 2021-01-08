9e0ec088216c3a9c26f7e5eed6c991d956da09c1
Hideaki Kawai <kayhide@gmail.com>
2021-01-08 20:33:04 +0900

    Add mnist example

--- shell.nix
+++ shell.nix
@@ -1,8 +1,18 @@
-{ nixpkgs ? import <nixpkgs> {}
-}:
-
 let
-  inherit (nixpkgs) pkgs;
+  nixpkgs-src = builtins.fetchTarball {
+    # master of 2020-11-21.
+    url = "https://github.com/NixOS/nixpkgs/archive/2247d824fe07f16325596acc7faa286502faffd1.tar.gz";
+    sha256 = "sha256:09jzdnsq7f276cfkmynqiaqg145k8z908rlr0170ld1sms1a83rw";
+  };
+
+  nixpkgs = import nixpkgs-src { config = { allowUnfree = true; }; };
+
+  inherit (nixpkgs) pkgs lib stdenv;
+
+  lib-path = lib.makeLibraryPath [
+    stdenv.cc.cc
+    pkgs.linuxPackages.nvidia_x11
+  ];
 
 in
 
@@ -10,4 +20,8 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     python3
   ];
+
+  shellHook = ''
+    export "LD_LIBRARY_PATH=${lib-path}"
+  '';
 }
